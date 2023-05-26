dc55a040ae84b4a14932ea7b400d339862e830f6
thiiagodantas <125404547+thiiagodantas@users.noreply.github.com>
2023-05-26 15:38:09 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,21 @@
+fun main() {
+  println("Olá mundo")
+  var nome: String = "Thi"
+  val nascimento: Int = 2005
+
+println(nome)          // exibe o nome
+println(nascimento)     // exibe o nascimento
+
+val myNum: Int = 5                // Int
+val myDoubleNum: Double = 5.99    // Double
+val myLetter: Char = 'D'          // Char
+val myBoolean: Boolean = true     // Boolean
+val myText: String = "Olá"      // String
+
+println (myNum)
+println (myDoubleNum)
+println (myLetter)
+println (myBoolean)
+println (myText)
+
+}
