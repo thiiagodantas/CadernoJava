6e27382850c68c20659c04a4ada13719f24d3179
thiiagodantas <125404547+thiiagodantas@users.noreply.github.com>
2023-06-06 17:43:03 -0300

    Create variaveisbooleana.kt

---
+++ variaveisbooleana.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  //criar uma variavel do tipo booleana(false true)
+  var nomeVariavel: Boolean = false
+  println(nomeVariavel)
+  var x: Boolean = false
+  println(x)
+  //if faz teste e retorna boolean
+
+  if (x){
+    println("verdadeiro")
+  }
+  else{
+    println("falso")
+  }
+  
+}
